4985026fa4f6ac4ccd729deaf9ea667136b5274c
Antonio Saladino <antoniosaladino7@users.noreply.github.com>
2025-12-14 18:09:17 +0100

    init rebel7 marketing landing

---
+++ src/i18n/index.js
@@ -0,0 +1,17 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import it from "./local/it.json";
+import en from "./local/en.json";
+
+i18n.use(initReactI18next).init({
+    resources: {
+        it: { translation: it },
+        en: { translation: en },
+    },
+    lng: "it",
+    fallbackLng: "it",
+    interpolation: { escapeValue: false },
+});
+
+export default i18n;
